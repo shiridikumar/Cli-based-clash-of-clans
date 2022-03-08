6ad26dc78f2bed046718bd059634dff88797f212
ShiridiKumar <p.shiridi@research.iiit.ac.in>
2022-03-08 16:25:39 +0530

    king and barabrian ready

--- src/main.py
+++ src/main.py
@@ -2,15 +2,21 @@ from colorama import Fore, Back, Style
 import time
 horizontal=("-"*140).center(160)
 vertical=(("|"+" "*140+"|").center(160)+"\n")*50
-print(horizontal)
-print(vertical,end="")
-print(horizontal)
+# print(horizontal)
+# print(vertical,end="")
+# print(horizontal)
 starty=10
 startx=2
-prevx=startx;prevy=starty
-for i in range(1,50):
-    print("\033["+str(prevx)+";"+str(prevy)+"H"+" "*len("hello"),end="")
-    prevx=startx+i;prevy=starty+i
-    print("\033["+str(prevx)+";"+str(prevy)+"H"+"hello")
-    time.sleep(0.3)
-print("\n\n")
+barbarian=("["+"B"+"]/")+"\n"+("/\\ \n")
+#("|"+"-"*4+"|")c
+print(barbarian)
+king=("_"*4)+"\n"+("{"+" K"+"}/")+("\n /\\ \n")
+
+print(king)
+# prevx=startx;prevy=starty
+# for i in range(1,50):
+#     prevx=startx+i;prevy=starty+i
+#     print("\033["+str(prevx)+";"+str(prevy)+"H"+"hello")
+#     time.sleep(0.3)
+#     print("\033["+str(prevx)+";"+str(prevy)+"H"+" "*len("hello"),end="")
+# print("\n\n")
